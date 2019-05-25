a62493bdd5fafc3a94812cd0e38bc3f9ecc2647b
munhyunsu <munhyunsu@gmail.com>
2019-05-26 07:34:14 +0900

    Wear to application

---
+++ app/src/main/java/org/cs_cnu/sleep_watch_alert/MainActivity.kt
@@ -0,0 +1,12 @@
+package org.cs_cnu.sleep_watch_alert
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
