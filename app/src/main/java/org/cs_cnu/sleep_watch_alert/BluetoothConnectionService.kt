24ffc3372656f751537acd6ef1475716527f7030
munhyunsu <munhyunsu@gmail.com>
2019-10-18 22:13:55 +0900

    Change struct

---
+++ app/src/main/java/org/cs_cnu/sleep_watch_alert/BluetoothConnectionService.kt
@@ -0,0 +1,56 @@
+package org.cs_cnu.sleep_watch_alert
+
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.content.Context
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class BluetoothConnectionService(val context: Context) {
+
+    private var eventListener : IBluetoothEventListener = EmptyBluetoothEventListener()
+    private val enableRequest = EnableRequest(context, eventListener)
+    private val discoverRequest = DiscoverRequest(context, eventListener)
+    private val pairRequest = PairRequest(context, eventListener)
+    private val connectionRequest = ConnectionRequest(context, eventListener)
+    private val audioConnectionRequest = AudioConnectionRequest(context, eventListener)
+
+    fun setBluetoothEventListener(listener: IBluetoothEventListener) {
+        eventListener = listener
+    }
+
+    fun enableBluetoothAdapter() {
+        enableRequest.enableluetooth()
+    }
+
+    fun disableBluetoothAdapter() {
+        enableRequest.disableBluetooth()
+    }
+
+    fun discoverDevices() {
+        discoverRequest.discover()
+    }
+
+    fun pairDevice(device : BluetoothDevice) {
+        pairRequest.pair(device)
+    }
+
+    fun connectDevice(device: BluetoothDevice) {
+        connectionRequest.conntect(device)
+    }
+
+    fun stopConnectDevice() {
+        connectionRequest.stopConnect()
+    }
+
+    fun connectAudioDevice(device : BluetoothDevice) {
+        audioConnectionRequest.connect(device)
+    }
+
+    fun cleanUp() {
+        enableRequest.cleanup()
+        discoverRequest.cleanup()
+        pairRequest.cleanup()
+    }
+
+}
--- app/src/main/java/org/cs_cnu/sleep_watch_alert/MainActivity.kt
+++ app/src/main/java/org/cs_cnu/sleep_watch_alert/MainActivity.kt
@@ -1,21 +1,14 @@
 package org.cs_cnu.sleep_watch_alert
 
-import android.bluetooth.BluetoothAdapter
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
+    private val bluetoothConnectionService : BluetoothConnectionService = BluetoothConnectionService(this)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-
-
-    fun getBluetoothAdapter() {
-        val bluetoothAdapter: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
-        if (bluetoothAdapter == null) {
-            // Device doesn't support Bluetooth
-        }
     }
 }
